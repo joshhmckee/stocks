9a300edbffe4afa2d47f8ee012e29d93727743a7
Josh McKee <j4mckee@uwaterloo.ca>
2023-02-17 19:50:20 -0500

    configs and main page design

--- main.py
+++ main.py
@@ -2,17 +2,41 @@ import streamlit as st
 import pandas as pd
 from macrotrends import MacroTrendsAPI
 
+# make window wide
+st.set_page_config(layout="wide")
+
 # initialize api
 mt = MacroTrendsAPI()
 
 st.header("Stock Dashboard")
 
-ticker = st.text_input('Ticker', 'TSLA')
+col1, _, _ = st.columns(3)
+with col1:
+    ticker = st.text_input('Ticker', 'TSLA')
 
-tab1, tab2 = st.tabs(["Revenue", "EPS"])
-with tab1:
-    data = mt.get_quarterly_revenue(ticker)
-    st.bar_chart(data)
-with tab2:
-    data = mt.get_quarterly_eps(ticker)
-    st.bar_chart(data)
+st.write('Graph Selections')
+col1, col2 = st.columns(2)
+with col1:
+    revenue_check = st.checkbox('Revenue')
+with col2:
+    eps_check = st.checkbox('EPS')
+
+# tab1, tab2 = st.tabs(["Revenue", "EPS"])
+# with tab1:
+#     data = mt.get_quarterly_revenue(ticker)
+#     st.bar_chart(data)
+# with tab2:
+#     data = mt.get_quarterly_eps(ticker)
+#     st.bar_chart(data)
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    if revenue_check:
+        data = mt.get_quarterly_revenue(ticker)
+        st.bar_chart(data)
+with col2:
+    if eps_check:
+        data = mt.get_quarterly_eps(ticker)
+        st.bar_chart(data)
+with col3:
+    pass
